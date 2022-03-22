2ebe99e6e41902c1023ff2ee48b0fec7eb4625f1
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-03-22 23:44:23 +0530

    yargs for argument parsing

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
 
 const validator=require('validator')
 const chalk=require('chalk')
+const yargs=require('yargs')
 
 const fname =require('./util.js')
 const add= require('./util.js')
+
 const fs=require('fs')
-const sum=add(4,-2)
-fs.copyFileSync
+//const sum=add(4,-2)
+//fs.copyFileSync
 
 // fs.writeFileSync('notes.txt','Hello sarthak!')
 
 // fs.appendFileSync('notes.txt'," ++  I am done with the task")
 
 console.log(chalk.red.bold("sarthak"))
+
+//console.log(process.argv)
+yargs.command({
+    command:'add',
+    describe:'add the notes',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Note content',
+            demandOption:true,
+            type:'string'
+        }
+
+    },
+    handler: function(argv)
+    {
+        console.log('Title :' + argv.title)
+        console.log('Body: '+ argv.body)
+    }
+})
+yargs.command({
+    command:'list',
+    describe:'lsit the notes',
+    handler: function()
+    {
+        console.log('lsiting notes')
+    }
+})
+yargs.command({
+    command:'read',
+    describe:'read the notes',
+    handler: function()
+    {
+        console.log('reading notes')
+    }
+})
+yargs.parse()
+ // console.log(yargs.argv)
+
 /*console.log(fname)
 console.log(sum)
 
