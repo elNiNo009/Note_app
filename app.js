8d24b267486b4d206aef38917b112c4c2c2c15e6
sarthak chauhan <sarthak.chauhan496@gmail.com>
2022-03-22 15:44:42 +0530

    nodemon global npm module

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 
 const validator=require('validator')
+const chalk=require('chalk')
 
 const fname =require('./util.js')
 const add= require('./util.js')
 const fs=require('fs')
 const sum=add(4,-2)
 fs.copyFileSync
+
 // fs.writeFileSync('notes.txt','Hello sarthak!')
 
 // fs.appendFileSync('notes.txt'," ++  I am done with the task")
 
-/*console.log("sarthak")
-console.log(fname)
+console.log(chalk.red.bold("sarthak"))
+/*console.log(fname)
 console.log(sum)
 
 const msg=require('./notes.js')
 const mess=msg()
 console.log(mess)
 */
-console.log(validator.isEmail('sarthak.chauhan@gmail.com'))
+
+//console.log(validator.isEmail('sarthak.chauhan@gmail.com'))
